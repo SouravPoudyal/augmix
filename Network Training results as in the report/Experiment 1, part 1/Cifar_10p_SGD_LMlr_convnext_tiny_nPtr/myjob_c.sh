7766baa50cbdbf09b7dae4c9695ee3c1aa2cb9ab
sourav <sourav.poudyal@student.uni-siegen.de>
2023-05-08 16:29:39 +0200

    commit 6

---
+++ Network Training results as in the report/Experiment 1, part 1/Cifar_10p_SGD_LMlr_convnext_tiny_nPtr/myjob_c.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --job-name=c
+#SBATCH --output=c.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sourav.poudyal@student.uni-siegen.de
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=04:00:00
+#SBATCH --mem=35GB
+#SBATCH --gres=gpu:1
+
+module load GpuModules
+module load pytorch-extra-py37-cuda11.2-gcc8/1.9.1
+
+python cifar_t2_c.py -m convnext_tiny
